3ff0e847e39134b225778dbf6b493a5fd6d52a0b
s-age <1254197+s-age@users.noreply.github.com>
2026-01-07 06:01:01 +0900

    feat: improve StartSessionPage

--- src/web/components/pages/StartSessionPage/style.css.ts
+++ src/web/components/pages/StartSessionPage/style.css.ts
@@ -3,13 +3,11 @@ import { style } from '@vanilla-extract/css'
 import { colors } from '@/styles/colors.css'
 
 export const pageContent = style({
-  display: 'flex',
   flex: 1,
   width: '60%',
   minHeight: 0,
   margin: '12px 0',
   borderRadius: '10px',
   backgroundColor: colors.gray,
-  flexDirection: 'column',
   alignSelf: 'center'
 })

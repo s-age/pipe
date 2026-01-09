d9e2019f4ff07d8c5b0599beef8939981fb61bf6
s-age <1254197+s-age@users.noreply.github.com>
2026-01-09 12:15:06 +0900

    chore: modify code style

--- src/web/components/molecules/Code/style.css.ts
+++ src/web/components/molecules/Code/style.css.ts
@@ -5,19 +5,13 @@ import { variables } from '@/styles/theme.css'
 export const code = style({
   fontFamily: variables.fontFamily.mono,
   fontSize: variables.fontSize.s,
-  backgroundColor: variables.color.backgroundCode,
-  padding: `${variables.spacing.xs} ${variables.spacing.s}`,
-  borderRadius: variables.borderRadius.s,
-  border: `1px solid ${variables.color.border}`
+  padding: `${variables.spacing.xs} ${variables.spacing.s}`
 })
 
 export const pre = style({
   fontFamily: variables.fontFamily.mono,
   fontSize: variables.fontSize.s,
-  backgroundColor: variables.color.backgroundCode,
   padding: variables.spacing.m,
-  borderRadius: variables.borderRadius.m,
-  border: `1px solid ${variables.color.border}`,
   overflow: 'auto',
   margin: 0
 })
